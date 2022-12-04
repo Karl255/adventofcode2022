3bdf988ca61b31b7c59b387e25cf9649c1d0c93e
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-12-04 09:19:22 +0100

    Day 04: improved golfing solution (using File.stream! now)

--- day-04/part-1-min.exs
+++ day-04/part-1-min.exs
@@ -1,4 +1,4 @@
 import String
 import MapSet
-throw Enum.count Enum.map(split(File.read!("i"),"
-"),fn l->Enum.map(split(l,","),fn r->[s,e]=Enum.map(split(r,"-"),&to_integer/1);new(s..e)end)end),fn[x,y]->subset?(x,y)||subset?(y,x)end
+throw Enum.count split(File.read!("i"),"
+"),fn l->[x,y]=Enum.map split(l,","),fn r->[s,e]=Enum.map split(r,"-"),&to_integer/1;new s..e end;subset?(x,y)||subset?y,x end
--- day-04/part-2-min.exs
+++ day-04/part-2-min.exs
@@ -1,3 +1,3 @@
 import String
-throw Enum.count Enum.map(split(File.read!("i"),"
-"),fn l->Enum.map(split(l,","),fn r->[s,e]=Enum.map(split(r,"-"),&to_integer/1);s..e end)end),fn[x,y]->!Range.disjoint?(x,y)end
+throw Enum.count split(File.read!("i"),"
+"),fn l->[x,y]=Enum.map split(l,","),fn r->[s,e]=Enum.map split(r,"-"),&to_integer/1;s..e end;!Range.disjoint?x, y end
