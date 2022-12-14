0b57ad076c5ee205bc2a361210dc9922a3fc0ee6
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-12-14 15:46:41 +0100

    Day 14: added solution in Elixir

---
+++ day-14/part-2.exs
@@ -0,0 +1,49 @@
+defmodule M do
+  def simulate_grain(walls, max_y, {x, y} \\ {500, 0}) do
+    cond do
+      y === max_y -> {x, y}
+      not MapSet.member?(walls, {x, y + 1}) -> simulate_grain(walls, max_y, {x, y + 1})
+      not MapSet.member?(walls, {x - 1, y + 1}) -> simulate_grain(walls, max_y, {x - 1, y + 1})
+      not MapSet.member?(walls, {x + 1, y + 1}) -> simulate_grain(walls, max_y, {x + 1, y + 1})
+      true -> {x, y}
+    end
+  end
+
+  def simulate_sand(walls, max_y, n \\ 0) do
+    case simulate_grain(walls, max_y) do
+      {500, 0} -> n + 1
+      grain -> simulate_sand(MapSet.put(walls, grain), max_y, n + 1)
+    end
+  end
+end
+
+walls =
+  File.stream!("i")
+  |> Enum.map(&String.trim/1)
+  |> Enum.map(fn polyline ->
+    [start | points] =
+      String.split(polyline, " -> ")
+      |> Enum.map(fn point ->
+        String.split(point, ",")
+        |> Enum.map(&String.to_integer/1)
+        |> List.to_tuple()
+      end)
+
+    Enum.reduce(points, {start, MapSet.new()}, fn {x, y} = point, {{from_x, from_y}, acc} ->
+      all_dots =
+        if from_x === x do
+          Enum.map(from_y..y, &{x, &1})
+        else
+          Enum.map(from_x..x, &{&1, y})
+        end
+
+      {point, MapSet.union(acc, MapSet.new(all_dots))}
+    end)
+    |> elem(1)
+  end)
+  |> Enum.reduce(&MapSet.union/2)
+
+{_, max_y} = Enum.max_by(walls, fn {_x, y} -> y end)
+
+M.simulate_sand(walls, max_y + 1)
+|> IO.puts()
