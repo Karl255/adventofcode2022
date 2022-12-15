b6109ab62e1826bdb5bd32f99e7bad5f09b1c222
Karl255 <49595438+Karl255@users.noreply.github.com>
2022-12-15 18:14:48 +0100

    Day 15: added even more optimized part 1 solution in Elixir

--- day-15/part-1.exs
+++ day-15/part-1.exs
@@ -1,3 +1,11 @@
+defmodule RangeHelpers do
+  def combinable(s1..e1 = r1, s2..e2 = r2) do
+    not Range.disjoint?(r1, r2) or
+      e1 + 1 === s2 or
+      e2 + 1 === s1
+  end
+end
+
 input =
   File.stream!("i")
   |> Enum.map(&String.trim/1)
@@ -14,31 +22,41 @@ input =
   end)
 
 sensors = Enum.map(input, &elem(&1, 0))
-beacons = Enum.map(input, &elem(&1, 1))
+beacons = Enum.map(input, &elem(&1, 1)) |> Enum.uniq()
 row = if length(sensors) === 14, do: 10, else: 2_000_000
 
-beacons_in_row =
-  Enum.filter(beacons, fn {_x, y} -> y === row end)
-  |> Enum.map(&elem(&1, 0))
-  |> MapSet.new()
+ranges =
+  sensors
+  |> Enum.map(fn {x, y} ->
+    dist =
+      beacons
+      |> Enum.map(fn {bx, by} ->
+        abs(bx - x) + abs(by - y)
+      end)
+      |> Enum.min()
 
-Enum.map(sensors, fn {x, y} ->
-  d =
-    Enum.map(beacons, fn {bx, by} ->
-      abs(bx - x) + abs(by - y)
-    end)
-    |> Enum.min()
+    col1 = x + dist - abs(row - y)
+    col2 = x - dist + abs(row - y)
 
-  {{x, y}, d}
-end)
-|> Enum.map(fn {{x, y}, dist} ->
-  col1 = x + dist - abs(row - y)
-  col2 = x - dist + abs(row - y)
-  # this could be further optimized by using a list of ranges, but this works well-enough for now
-  # if col1 < col2, do: col1..col2, else: col2..col1
-  MapSet.new(col1..col2)
-end)
-|> Enum.reduce(&MapSet.union/2)
-|> MapSet.difference(beacons_in_row)
-|> MapSet.size()
-|> IO.inspect(syntax_colors: IO.ANSI.syntax_colors())
+    if col1 < col2, do: col1..col2, else: col2..col1
+  end)
+  |> Enum.sort_by(fn s.._ -> s end)
+  |> Enum.map(fn r -> [r] end)
+  |> Enum.reduce(fn [s..e], [last_s..last_e | acc_rest] = acc ->
+    if RangeHelpers.combinable(s..e, last_s..last_e) do
+      bigger = max(e, last_e)
+      smaller = min(s, last_s)
+      [smaller..bigger | acc_rest]
+    else
+      [s..e | acc]
+    end
+  end)
+  |> Enum.reverse()
+
+not_a_beacon =
+  Enum.map(ranges, &Range.size(&1))
+  |> Enum.sum()
+
+beacons_in_row = Enum.count(beacons, fn {_x, y} -> y === row end)
+
+IO.puts(not_a_beacon - beacons_in_row)
